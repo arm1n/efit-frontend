a195189da232b8b791301aaf5ebdc62174882042
Armin Pfurtscheller <armin.pfurtscheller@googlemail.com>
2017-09-11 22:23:44 +0200

    tabber: revert angular rendering

--- src/js/app/directives/tabber.js
+++ src/js/app/directives/tabber.js
@@ -20,7 +20,7 @@
     this.$transclude = $transclude;
 
     this.domId = 'tabber-' + $scope.$id;
-    this.activeTab = this.tabDescription;
+
     this.hasExercise = $transclude.isSlotFilled('exercise');
   };
 
@@ -30,54 +30,8 @@
   // PROPERTIES
   //
 
-  /** @var {object} activeTab Reference to current active tab. */
-  Tabber.prototype.activeTab = null;
-
   /** @var {array} hasExercise If slot `exercise` has contents. */
   Tabber.prototype.hasExercise = false;
-
-  /** @var {string} tabExercise Id of `exercise` tab. */
-  Tabber.prototype.tabExercise = 'exercise';
-
-  /** @var {string} tabDescription Id of `description` tab. */
-  Tabber.prototype.tabDescription = 'description';
-
-  //
-  // METHODS
-  //
-
-  /**
-   * Sets up event listeners and animation frame.
-   *
-   * @public
-   * @method onClick
-   * @return {void}
-   */
-  Tabber.prototype.onClick = function(tab) {
-    this.activeTab = tab;
-  };
-
-  /**
-   * Sets up event listeners and animation frame.
-   *
-   * @public
-   * @method onClick
-   * @return {void}
-   */
-  Tabber.prototype.isDescriptionActive = function() {
-    return this.activeTab === this.tabDescription;
-  };
-
-  /**
-   * Sets up event listeners and animation frame.
-   *
-   * @public
-   * @method onClick
-   * @return {void}
-   */
-  Tabber.prototype.isExerciseActive = function() {
-    return this.activeTab === this.tabExercise;
-  };
 
   //
   // REGISTRY
